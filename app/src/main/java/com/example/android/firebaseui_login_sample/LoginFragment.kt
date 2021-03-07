9201001d6b7b015b1632d04e1cd61a41d742d590
miro <minev.miro@gmail.com>
2021-03-07 22:54:10 +0200

    navigation based on state

--- app/src/main/java/com/example/android/firebaseui_login_sample/LoginFragment.kt
+++ app/src/main/java/com/example/android/firebaseui_login_sample/LoginFragment.kt
@@ -20,20 +20,18 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.activity.addCallback
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
-import com.example.android.firebaseui_login_sample.databinding.*
+import androidx.navigation.fragment.findNavController
+import com.example.android.firebaseui_login_sample.databinding.FragmentLoginBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
@@ -50,7 +48,7 @@ class LoginFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         // Inflate the layout for this fragment.
         val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
@@ -58,6 +56,11 @@ class LoginFragment : Fragment() {
         )
 
         binding.authButton.setOnClickListener { launchSignInFlow() }
+
+        // If the user presses the back button, bring them back to the home screen
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            navController.popBackStack(R.id.mainFragment, false)
+        }
 
         return binding.root
     }
